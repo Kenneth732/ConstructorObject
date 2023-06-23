0494503893f7b82700743d4372f34cbdd50d99d4
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-23 09:42:47 +0300

    contacts array which iiin real worl senerial needs to be empty to get data from inputs

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function Contact(name, phoneNumber, email) {
   this.email = email;
 }
 
+// Sample contact data
+var contacts = [
+  new Contact("John Doe", "1234567890", "john@example.com"),
+  new Contact("Jane Smith", "0987654321", "jane@example.com"),
+  // Add more sample contacts here
+];
+
+
